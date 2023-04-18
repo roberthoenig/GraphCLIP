4bf6eaf5f2ee99e4c16e86f0f32c45dc63759e2c
Robert Hönig <robhoenig@gmail.com>
2023-04-18 23:16:04 +0200

    Add TextCLIP evaluation on ChatGPT captions.

--- datasets/mscoco.py
+++ datasets/mscoco.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 class MSCOCO:
-    def __init__(self, n_samples, image_path, ann_path, id_path):
+    def __init__(self, n_samples, image_path, ann_path, id_path,  caption_path=None):
         with open(ann_path,  'r') as f:
             ann = json.load(f)
         with open(id_path, 'r') as f:
@@ -14,12 +14,31 @@ class MSCOCO:
         df = pd.DataFrame.from_dict(ann['annotations'])
         images   = []
         captions = []
-        for image_id, group in df.groupby('image_id'):
-            if image_id not in ids:
-                continue
-            images.append(imageid2filename[image_id])
-            captions.append(list(group['caption'])[:5])
-            if len(images) >= n_samples:
-                break
-        self.img_paths   = [os.path.join(image_path, image) for image in images]
+        if caption_path is None:
+            for image_id, group in df.groupby('image_id'):
+                if image_id not in ids:
+                    continue
+                images.append(imageid2filename[image_id])
+                captions.append(list(group['caption'])[:5])
+                if len(images) >= n_samples:
+                    break
+        else:
+            with open(caption_path, 'r') as f:
+                captions_j = json.load(f)
+            # WARNING: There are several samples with identical coco_id's. The following
+            # dictionary comprehension only keeps the last of them.
+            print(captions_j[0])
+            captions_d = {c['coco_id']: c['captions']['short'] if not c.get('info', None)=='Empty Graph!' else "d" for c in captions_j}
+            captions = []
+            for image_id, group in df.groupby('image_id'):
+                if image_id not in ids:
+                    continue
+                maybe_caption = captions_d[image_id]
+                if maybe_caption is None:
+                    continue
+                images.append(imageid2filename[image_id])
+                captions.append([maybe_caption])
+                if len(images) >= n_samples:
+                    break
+        self.img_paths = [os.path.join(image_path, image) for image in images]
         self.captions = captions
